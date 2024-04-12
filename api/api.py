e5f083bebc91380998200699ac95a932b3ac1df4
alekseyteleshko@gmail.com <alekseyteleshko@gmail.com>
2024-04-12 21:43:43 +0200

    up4

--- api/api.py
+++ api/api.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
@@ -15,9 +17,20 @@ print("\nKeys:", len(repo_dict))
 for key in sorted(repo_dict.keys()):
  print(key)
 print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
+ names.append(repo_dict['name'])
+ stars.append(repo_dict['stargazers_count'])
  print('\nName:', repo_dict['name'])
  print('Owner:', repo_dict['owner']['login'])
  print('Stars:', repo_dict['stargazers_count'])
  print('Repository:', repo_dict['html_url'])
  print('Description:', repo_dict['description'])
+
+
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
